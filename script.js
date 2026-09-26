e1fede2e36f6a06e80604614a800d8581427b5f5
melidottori-cell <melidottori@gmail.com>
2026-09-26 13:02:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,32 @@ if (infoExtra.classList.contains(oculto))
 
 botonesVerMas.forEach(function(boton)
 {boton.addEventListener('click' , alternarInformacion);})
+
+const slides= document.querySelectorAll('.slide');
+const dots= document.querySelectorAll('.dot');
+const prevBtn= document.getElementById('.prevBtn');
+const nextBtn= document.getElementById('.nextBtn');
+const carouselContainer= document.getElementById('.carousel');
+
+let currentSlide= 0;
+let autoSlideInterval;
+
+function showSlide (index)
+{if (slides.length === 0) return;
+if (index>= SLIDES.LENGTH)
+{currentSlide= slides.length -1;}
+else {currentSlide =index;}
+}
+slides [currentSlide] .classList.add(active);
+
+function  nextLide (){
+    showSlide(currentSlide + 1);
+}
+function prevSlide(){
+    showSlide(currentSlide - 1);}
+
+    if (nextBtn) nextBtn.addEventListener(
+       'click', nextSlide);
+ if (prevBtn) prevBtn.addEventListener(
+       'click', nextSlide);
+    
